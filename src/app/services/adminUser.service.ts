7cc9924af5ae9a2ca07a95d6cea3e198ab39eab3
Julian Meier <julianmeier@hotmail.com>
2026-01-27 17:41:01 -0300

    Update form to handle edit user and cancel actions

--- src/app/services/adminUser.service.ts
+++ src/app/services/adminUser.service.ts
@@ -79,33 +79,5 @@ export class AdminUserService {
     }
   }
 
-
-
-
-  // async createUser(data: CreateUserByAdminRequest): Promise<CreateUserResponse> {
-
-  //   const token = localStorage.getItem('authToken');
-  //   const headers = new HttpHeaders({
-  //     Authorization: `Bearer ${token}`
-  //   });
-
-
-  //   try {
-  //     const response = await firstValueFrom(
-  //       this.http.post<CreateUserResponse>(`${this.API_URL}`, data, { headers })
-  //     );
-  //     console.log('Usuario creado:', response.message);
-  //     return response;
-  //   } catch (error: any) {
-  //     console.error('Admin create user error:', error);
-  //     const serverErrorMessage =
-  //       error.error?.message ||
-  //       (error.error?.errors ? JSON.stringify(error.error.errors) : null) ||
-  //       'Error al crear usuario. Intenta nuevamente.';
-  //     throw serverErrorMessage;
-  //   }
-  // }
-
-
   
 }
